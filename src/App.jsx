2e2f9a32e89ba75266bcd870c38e18ab2d5eae37
shubham <shubhamchaudhari707@gmail.com>
2024-10-14 11:12:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import KwikNote from './components/KwikNote';
+
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+
+  const addNote = () => {
+    setNotes([...notes, { id: Date.now(), text: 'click edit to write your note...' }])
+  }
+
+  const updateNote = (id, newText) => {
+    setNotes((prevNote) => prevNote.map((note) => note.id === id ? { ...note, text: newText } : note))
+  }
+
+  const deleteNote = (id) => {
+    setNotes(notes.filter((note) => note.id !== id))
+  }
+
+
+  return (
+    <>
+      <h1 className='m-3 ml-5 text-white font-bold text-5xl italic'>Kwik Note</h1>
+
+      <div className='text-center'>
+        <button onClick={addNote} className='text-white bg-green-700 hover:bg-green-800 px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 rounded-lg'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+        </button>
+      </div>
+
+      <div className='flex flex-wrap'>
+        {
+          notes.map((note) => {
+            return (
+              <>
+                <KwikNote key={note.id} id={note.id} text={note.text} onUpdate={updateNote} onDelete={() => deleteNote(note.id)} />
+              </>
+            )
+          })
+        }
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
